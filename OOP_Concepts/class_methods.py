bca9d885916a51a3f7ae97b06fdebfd452335fa7
DIJA <a@fs-MacBook-Pro.local>
2024-11-17 22:47:44 +0000

    Define BankAccount class with methods for deposit and withdrawal, implement static and class methods to get the bank policy and bank name, and add demostration of creating instances and calling methods

---
+++ OOP_Concepts/class_methods.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+class BankAccount:
+    """class variable for the bank name"""
+    bank_name = "Tech4Girls Bank"
+
+    def __init__(self, account_holder,):
+        """instance variables for the account holder's name and balance initialized to 0"""
+        self.account_holder = account_holder
+        self.balance = 0
+
+    def deposit(self,amount):
+        """add money to the account"""
+        self.balance += amount
+        print(f"{amount} has been deposited into your account\nCurrent balance: {self.balance}")
+        
+    def withdraw(self, amount):
+        """deduct money from the account and ensuring balance doesn't go negative"""
+        if self.balance < amount:
+            print("Insufficient funds for this withdrawal")
+        else:
+           self.balance -= amount
+           print(f"You have withdrawn {amount} from your account\nCurrent balance: {self.balance}")
+
+
+    @staticmethod
+    def bank_policy():
+        """print a generic policy message"""
+        print("Welcome to Tech4Girls Bank. We are committed to providing a safe and secure banking experience for all our customers.")
+
+    @classmethod
+    def get_bank_name(cls,):
+        """return the bank name"""
+        return cls.bank_name
+    
+"""Demostrating instances for the BankAccount class"""
+
+bankaccount1 = BankAccount("Linda")
+bankaccount1.bank_policy()
+bankaccount1.deposit(1000)
+bankaccount1.withdraw(500)
+
+"""withdrawing more than the amount"""
+bankaccount1.withdraw(1500)
+print(bankaccount1.get_bank_name())
+
+"""create an instance for Alex"""
+bankaccount2 = BankAccount("Alex")
+bankaccount2.bank_policy()
+bankaccount2.deposit(800)
+bankaccount2.withdraw(300)
+print(bankaccount2.get_bank_name())
+
